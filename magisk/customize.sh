bcbb64dec10a1ffe173ff539d400971176f6d66b
chenzyadb <89340470+chenzyadb@users.noreply.github.com>
2024-03-29 21:31:18 +0800

    Update V18(20240329)

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -1,88 +1,90 @@
+#!/system/bin/sh
+
 ui_print "- CuprumTurbo Scheduler Module"
 ui_print "- Installing..."
 
 chmod -R 0777 ${MODPATH}/
 
-function get_pineapple_name() {
-    cpu7_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
-    if [ $cpu7_max_freq -gt 3200000 ]; then
+get_pineapple_name() {
+    cpu_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
+    if [ "$cpu_max_freq" -gt 3200000 ]; then
         echo "sdm8gen3"
     else
         echo "sdm7+gen3"
     fi
 }
 
-function get_taro_name() {
+get_taro_name() {
     cpu7_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
     gpu_max_freq=$(cat /sys/class/kgsl/kgsl-3d0/max_clock_mhz)
-    # SDM8+GEN1: 2.0+2.75+3.19 SDM8GEN1/8+GEN1UC: 1.8+2.5+3.0 SDM7+GEN2: 1.8+2.5+2.91.
-    if [ $cpu7_max_freq -gt 3100000 ]; then
+    if [ "$cpu7_max_freq" -gt 3100000 ]; then
         echo "sdm8+gen1"
-    elif [ $cpu7_max_freq -gt 2950000 ]; then
-        if [ $gpu_max_freq -gt 850 ]; then
-            # Adreno730 @ 900MHz
+    elif [ "$cpu7_max_freq" -gt 2950000 ]; then
+        if [ "$gpu_max_freq" -gt 850 ]; then
             echo "sdm8+gen1"
         else
-            # Adreno730 @ 818MHz
             echo "sdm8gen1"
         fi
-    elif [ $cpu7_max_freq -gt 2900000 ]; then
+    elif [ "$cpu7_max_freq" -gt 2900000 ]; then
         echo "sdm7+gen2"
     else
         echo "sdm7gen1"
     fi
 }
 
-function get_lahaina_name() {
+get_lahaina_name() {
     cpu7_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
     cpu4_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy4/cpuinfo_max_freq)
-    if [ $cpu7_max_freq -gt 2800000 ]; then
+    if [ "$cpu7_max_freq" -gt 2800000 ]; then
         echo "sdm888"
-    elif [ $cpu4_max_freq -gt 2300000 ]; then
+    elif [ "$cpu4_max_freq" -gt 2300000 ]; then
         echo "sdm778"
     else
         echo "sdm780"
     fi
 }
 
-function get_lito_name() {
+get_lito_name() {
     cpu_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
-    if [ $cpu_max_freq -gt 2300000 ]; then
+    if [ "$cpu_max_freq" -gt 2300000 ]; then
         echo "sdm765"
     else
         echo "sdm750"
     fi
 }
 
-function get_sm6150_name() {
+get_sm6150_name() {
     cpu_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
-    if [ $cpu_max_freq -gt 2200000 ]; then
+    if [ "$cpu_max_freq" -gt 2200000 ]; then
         echo "sdm730"
     else
         echo "sdm675"
     fi
 }
 
-function get_mt6895_name() {
+get_mt6895_name() {
     cpu_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
-    if [ $cpu_max_freq -gt 3000000 ]; then
+    if [ "$cpu_max_freq" -gt 3000000 ]; then
         echo "dimensity8200"
     else
         echo "dimensity8100"
     fi
 }
 
-function get_bengal_name() {
+get_bengal_name() {
     cpu_max_freq=$(cat /sys/devices/system/cpu/cpufreq/policy7/cpuinfo_max_freq)
-    if [ $cpu_max_freq -gt 2300000 ]; then
+    if [ "$cpu_max_freq" -gt 2300000 ]; then
         echo "sdm680"
     else
         echo "sdm665"
     fi
 }
 
-function get_config_name() {
+get_config_name() {
     case "$1" in
+    crow*)
+        echo "sdm7gen3"
+        ;;
     garnet*)
         echo "sdm6gen1"
         ;;
@@ -105,7 +107,6 @@ function get_config_name() {
         get_taro_name
         ;;
     lahaina*)
-        # snapdragon 888/778/780
         get_lahaina_name
         ;;
     shima*)
@@ -132,14 +133,19 @@ function get_config_name() {
     sm6150*)
         get_sm6150_name
         ;;
+    sdm670*)
+        echo "sdm710"
+        ;;
     sdm710*)
         echo "sdm710"
+        ;;
+    sdm439*)
+        echo "sdm439"
         ;;
     sdm450*)
         echo "sdm625"
         ;;
     sdm4350*)
-        # snapdragon 480
         echo "sdm730"
         ;;
     msm8953*)
@@ -151,15 +157,19 @@ function get_config_name() {
     sdm636*)
         echo "sdm660"
         ;;
+    sdm632*)
+        echo "sdm660"
+        ;;
+    sdm630*)
+        echo "sdm630"
+        ;;
     trinket*)
         echo "sdm665"
         ;;
     bengal*)
-        # snapdragon 665/460/680
         get_bengal_name
         ;;
     holi*)
-        # snapdragon 695
         echo "sdm4gen1"
         ;;
     msm8998*)
@@ -168,38 +178,16 @@ function get_config_name() {
     msm8996*)
         echo "sdm820"
         ;;
-#    universal9925*)
-#        echo "exynos2200"
-#        ;;
-#    universal2100*)
-#        echo "exynos2100"
-#        ;;
-#    universal1080*)
-#        echo "exynos1080"
-#        ;;
-#    universal990*)
-#        echo "exynos990"
-#        ;;
-#    universal9825*)
-#        echo "exynos9825"
-#        ;;
-#    universal9820*)
-#        echo "exynos9820"
-#        ;;
     mt6771*)
-        # Helio P60/P70
         echo "helio_p60"
         ;;
     mt6779*)
-        # Helio P90
         echo "helio_g80"
         ;;
     mt6762*)
-        # Helio G25/P22
         echo "helio_p35"
         ;;
     mt6765*)
-        # Helio G35/P35
         echo "helio_p35"
         ;;
     mt6768*)
@@ -242,7 +230,6 @@ function get_config_name() {
         echo "dimensity1100"
         ;;
     mt6895*)
-        # Dimensity8200 also named mt6895(k6895v1_64)
         get_mt6895_name
         ;;
     mt6983*)
@@ -285,7 +272,6 @@ function get_config_name() {
         echo "unisoc_t770"
         ;;
     ums9230*)
-        # Unisoc T606
         echo "unisoc_t618"
         ;;
     *)
@@ -294,12 +280,12 @@ function get_config_name() {
     esac
 }
 
-platform_name=$(getprop ro.board.platform)
-config_name=$(get_config_name $platform_name)
+platform_name=$(getprop "ro.board.platform")
+config_name=$(get_config_name "$platform_name")
 
-if [ -e ${MODPATH}/configs/${config_name}.json ]; then
-    cp -f ${MODPATH}/configs/${config_name}.json ${MODPATH}/config.json
-    rm -rf ${MODPATH}/configs/
+if [ -f "${MODPATH}/configs/${config_name}.json" ]; then
+    cp -f "${MODPATH}/configs/${config_name}.json" "${MODPATH}/config.json"
+    rm -rf "${MODPATH}/configs/"
 
     ui_print "- ${platform_name} is supported."
     ui_print "- install finished."
--- magisk/service.sh
+++ magisk/service.sh
@@ -1,60 +1,59 @@
 #!/system/bin/sh
+BASE_DIR=$(dirname "$0")
 
-function get_min_freq() {
-	first_freq=$(cat "$1" | tr " " "\n" | head -1)
-	last_freq=$(cat "$1" | tr " " "\n" | tail -1)
-	if [ $first_freq -lt $last_freq ]; then
-		echo "${first_freq}"
+get_min_freq() {
+  freq_table=$(tr " " "\n" < "$1")
+	first_freq=$(echo "$freq_table" | head -1)
+	last_freq=$(echo "$freq_table" | tail -1)
+	if [ "$first_freq" -lt "$last_freq" ]; then
+		echo "$first_freq"
 	else
-		echo "${last_freq}"
+		echo "$last_freq"
 	fi
 }
 
-function get_max_freq() {
-	first_freq=$(cat "$1" | tr " " "\n" | head -1)
-	last_freq=$(cat "$1" | tr " " "\n" | tail -1)
-	if [ $first_freq -gt $last_freq ]; then
-		echo "${first_freq}"
-	else
-		echo "${last_freq}"
-	fi
+get_max_freq() {
+	freq_table=$(tr " " "\n" < "$1")
+  first_freq=$(echo "$freq_table" | head -1)
+  last_freq=$(echo "$freq_table" | tail -1)
+  if [ "$first_freq" -gt "$last_freq" ]; then
+  	echo "$first_freq"
+  else
+  	echo "$last_freq"
+  fi
 }
 
-function write_value() {
+write_value() {
 	for file in $2; do
-		if [ -e "${file}" ]; then
-			echo "$1" >"${file}"
+		if [ -f "$file" ]; then
+			echo "$1" >"$file"
 		fi
 	done
 }
 
-function lock_value() {
+lock_value() {
 	for file in $2; do
-		if [ -e "${file}" ]; then
-			chown root:root "${file}"
-			chmod 0666 "${file}"
-			echo "$1" >"${file}"
-			chmod 0444 "${file}"
+		if [ -f "$file" ]; then
+			chown root:root "$file"
+			chmod 0666 "$file"
+			echo "$1" >"$file"
+			chmod 0444 "$file"
 		fi
 	done
 }
 
-function change_task_cpuset() {
-	task_name=$1
-	cgroup=$2
-	for task_pid in $(ps -Ao pid,args | grep "${task_name}" | awk '{print $1}'); do
-		echo "${task_pid}" >"/dev/cpuset/${cgroup}/cgroup.procs"
+change_task_cpuset() {
+	for pid in $(pgrep -f "$1"); do
+		echo "$pid" >"/dev/cpuset/${2}/cgroup.procs"
 	done
 }
 
-function change_task_sched() {
-	task_name=$1
-	cgroup=$2
-	for task_pid in $(ps -Ao pid,args | grep "${task_name}" | awk '{print $1}'); do
+change_task_sched() {
+	for pid in $(pgrep -f "$1"); do
 		if [ -d /dev/stune ]; then
-			echo "${task_pid}" >"/dev/stune/${cgroup}/cgroup.procs"
+			echo "$pid" >"/dev/stune/${2}/cgroup.procs"
 		elif [ -d /dev/cpuctl ]; then
-			echo "${task_pid}" >"/dev/cpuctl/${cgroup}/cgroup.procs"
+			echo "$pid" >"/dev/cpuctl/${2}/cgroup.procs"
 		fi
 	done
 }
@@ -64,8 +63,8 @@ stop oneplus_brain_service 2>/dev/null
 stop vendor.perfservice 2>/dev/null
 stop perfd 2>/dev/null
 
-setprop persist.sys.hardcoder.name "" 2>/dev/null
-setprop persist.miui.miperf.enable "false" 2>/dev/null
+setprop "persist.sys.hardcoder.name" "" 2>/dev/null
+setprop "persist.miui.miperf.enable" "false" 2>/dev/null
 
 lock_value "1" "/proc/ppm/enabled"
 lock_value "0" "/proc/hps/enabled"
@@ -80,7 +79,7 @@ lock_value "0" "/sys/kernel/debug/fpsgo/common/force_onoff"
 lock_value "0" "/sys/kernel/fpsgo/common/force_onoff"
 lock_value "0" "/sys/kernel/ged/hal/dcs_mode"
 lock_value "enable: 0" "/proc/perfmgr/tchbst/user/usrtch"
-if [ -e /proc/ppm/policy_status ]; then
+if [ -f "/proc/ppm/policy_status" ]; then
     for ppm_policy in $(seq 0 10); do
         write_value "${ppm_policy} 1" "/proc/ppm/policy_status"
     done
@@ -96,8 +95,8 @@ if [ -e /proc/ppm/policy_status ]; then
        PPM_POLICY_SYS_BOOST
        PPM_POLICY_HICA"
     for policy in $disable_policy_list; do
-        IDX_INFO=$(cat /proc/ppm/policy_status | grep "${policy}")
-        IDX_NUM=$(echo ${IDX_INFO:1:1})
+        IDX_INFO=$(grep "$policy" < "/proc/ppm/policy_status")
+        IDX_NUM=$(expr substr "$IDX_INFO" 1 1)
         write_value "${IDX_NUM} 0" "/proc/ppm/policy_status"
     done
 fi
@@ -140,99 +139,89 @@ lock_value "0" "/proc/mz_scheduler/vip_task/enabled"
 lock_value "0" "/proc/sys/fbg/frame_boost_enabled"
 lock_value "0" "/proc/sys/fbg/input_boost_enabled"
 lock_value "0" "/proc/sys/fbg/slide_boost_enabled"
+lock_value "0" "/proc/sys/kernel/sched_util_clamp_min"
+lock_value "1024" "/proc/sys/kernel/sched_util_clamp_max"
+lock_value "0" "/proc/sys/kernel/*boost*"
+lock_value "" "/proc/sys/kernel/sched_lib_name"
+lock_value "0" "/proc/sys/walt/*boost*"
 lock_value "0" "/proc/sys/walt/input_boost/*"
+lock_value "" "/proc/sys/walt/sched_lib_name"
 
-for sched_type in walt kernel; do
-    lock_value "0" "/proc/sys/${sched_type}/sched_boost"
-    lock_value "0" "/proc/sys/${sched_type}/sched_util_clamp_min"
-    lock_value "" "/proc/sys/${sched_type}/sched_lib_name"
-done
-
-if [ -d /dev/stune/ ]; then
+if [ -d "/dev/stune/" ]; then
     lock_value "0" "/dev/stune/schedtune.boost"
     lock_value "0" "/dev/stune/schedtune.prefer_idle"
-    for stune_dir in /dev/stune/*; do
-        lock_value "0" "${stune_dir}/schedtune.prefer_idle"
-        lock_value "0" "${stune_dir}/schedtune.boost"
-        lock_value "0" "${stune_dir}/schedtune.sched_boost_no_override"
-    done
+    lock_value "0" "/dev/stune/*/schedtune.prefer_idle"
+    lock_value "0" "/dev/stune/*/schedtune.boost"
+    lock_value "0" "/dev/stune/*/schedtune.sched_boost_no_override"
 fi
 
-if [ -d /dev/cpuctl/ ]; then
-    lock_value "0" "/dev/cpuctl/cpu.uclamp.sched_boost_no_override"
-    lock_value "0" "/dev/cpuctl/cpu.uclamp.min"
-    lock_value "0" "/dev/cpuctl/cpu.uclamp.latency_sensitive"
+if [ -d "/dev/cpuctl/" ]; then
+    lock_value "0" "/dev/cpuctl/cpu.idle"
     lock_value "1024" "/dev/cpuctl/cpu.shares"
-    for cpuctl_dir in /dev/cpuctl/*; do
-        lock_value "0" "${cpuctl_dir}/cpu.uclamp.latency_sensitive"
-        lock_value "0" "${cpuctl_dir}/cpu.uclamp.min"
-        lock_value "0" "${cpuctl_dir}/cpu.uclamp.sched_boost_no_override"
-        lock_value "1024" "${cpuctl_dir}/cpu.shares"
-    done
+    lock_value "0" "/dev/cpuctl/*/cpu.uclamp.latency_sensitive"
+    lock_value "0" "/dev/cpuctl/*/cpu.uclamp.sched_boost_no_override"
+    lock_value "0" "/dev/cpuctl/*/cpu.uclamp.min"
+    lock_value "max" "/dev/cpuctl/*/cpu.uclamp.max"
+    lock_value "0" "/dev/cpuctl/*/cpu.idle"
+    lock_value "1024" "/dev/cpuctl/*/cpu.shares"
 fi
 
-if [ -d /proc/perfmgr/boost_ctrl/eas_ctrl/ ]; then
-    for perfserv_boost in /proc/perfmgr/boost_ctrl/eas_ctrl/perfserv_*_boost; do
-        lock_value "0" "${perfserv_boost}"
-    done
-    for perfserv_uclamp_min in /proc/perfmgr/boost_ctrl/eas_ctrl/perfserv_*_uclamp_min; do
-        lock_value "0" "${perfserv_uclamp_min}"
-    done
+if [ -d "/proc/perfmgr/boost_ctrl/eas_ctrl/" ]; then
+    lock_value "0" "/proc/perfmgr/boost_ctrl/eas_ctrl/perfserv_*_boost"
+    lock_value "0" "/proc/perfmgr/boost_ctrl/eas_ctrl/perfserv_*_uclamp_min"
 fi
 
-all_cpus=$(cat /dev/cpuset/cpus)
-for cpuset_cpus in /dev/cpuset/*/cpus; do
-    lock_value "${all_cpus}" "${cpuset_cpus}"
-done
+lock_value "$(cat /dev/cpuset/cpus)" "/dev/cpuset/*/cpus"
 
 lock_value "1" "/sys/devices/system/cpu/cpu*/online"
 lock_value "1" "/sys/devices/system/cpu/cpu*/core_ctl/enable"
 POLICY2_PATH="/sys/devices/system/cpu/cpu2/core_ctl"
 POLICY3_PATH="/sys/devices/system/cpu/cpu3/core_ctl"
 POLICY4_PATH="/sys/devices/system/cpu/cpu4/core_ctl"
+POLICY5_PATH="/sys/devices/system/cpu/cpu5/core_ctl"
 POLICY6_PATH="/sys/devices/system/cpu/cpu6/core_ctl"
 POLICY7_PATH="/sys/devices/system/cpu/cpu7/core_ctl"
-if [ -d ${POLICY2_PATH} ] && [ -d ${POLICY4_PATH} ] && [ -d ${POLICY7_PATH} ]; then #2+2+3+1
+if [ -d "$POLICY2_PATH" ] && [ -d "$POLICY5_PATH" ] && [ -d "$POLICY7_PATH" ]; then #2+3+2+1
     lock_value "2" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "2" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
-    lock_value "2" "/sys/devices/system/cpu/cpu2/core_ctl/max_cpus"
-    lock_value "2" "/sys/devices/system/cpu/cpu2/core_ctl/min_cpus"
-    lock_value "3" "/sys/devices/system/cpu/cpu4/core_ctl/max_cpus"
-    lock_value "3" "/sys/devices/system/cpu/cpu4/core_ctl/min_cpus"
+    lock_value "3" "/sys/devices/system/cpu/cpu2/core_ctl/max_cpus"
+    lock_value "3" "/sys/devices/system/cpu/cpu2/core_ctl/min_cpus"
+    lock_value "2" "/sys/devices/system/cpu/cpu5/core_ctl/max_cpus"
+    lock_value "2" "/sys/devices/system/cpu/cpu5/core_ctl/min_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/max_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/min_cpus"
-elif [ -d ${POLICY2_PATH} ]; then #2+2
+elif [ -d "$POLICY2_PATH" ]; then #2+2
     lock_value "2" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "2" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
     lock_value "2" "/sys/devices/system/cpu/cpu2/core_ctl/max_cpus"
     lock_value "2" "/sys/devices/system/cpu/cpu2/core_ctl/min_cpus"
-elif [ -d ${POLICY3_PATH} ] && [ -d ${POLICY7_PATH} ]; then #3+4+1
+elif [ -d "$POLICY3_PATH" ] && [ -d "$POLICY7_PATH" ]; then #3+4+1
     lock_value "3" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "3" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
     lock_value "4" "/sys/devices/system/cpu/cpu3/core_ctl/max_cpus"
     lock_value "4" "/sys/devices/system/cpu/cpu3/core_ctl/min_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/max_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/min_cpus"
-elif [ -d ${POLICY4_PATH} ] && [ -d ${POLICY7_PATH} ]; then #4+3+1
+elif [ -d "$POLICY4_PATH" ] && [ -d "$POLICY7_PATH" ]; then #4+3+1
     lock_value "4" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "4" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
     lock_value "3" "/sys/devices/system/cpu/cpu4/core_ctl/max_cpus"
     lock_value "3" "/sys/devices/system/cpu/cpu4/core_ctl/min_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/max_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/min_cpus"
-elif [ -d ${POLICY4_PATH} ]; then #4+4
+elif [ -d "$POLICY4_PATH" ]; then #4+4
     lock_value "4" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "4" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
     lock_value "4" "/sys/devices/system/cpu/cpu4/core_ctl/max_cpus"
     lock_value "4" "/sys/devices/system/cpu/cpu4/core_ctl/min_cpus"
-elif [ -d ${POLICY6_PATH} ] && [ -d ${POLICY7_PATH} ]; then #6+1+1
+elif [ -d "$POLICY6_PATH" ] && [ -d "$POLICY7_PATH" ]; then #6+1+1
     lock_value "6" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "6" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu6/core_ctl/max_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu6/core_ctl/min_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/max_cpus"
     lock_value "1" "/sys/devices/system/cpu/cpu7/core_ctl/min_cpus"
-elif [ -d ${POLICY6_PATH} ]; then #6+2
+elif [ -d "$POLICY6_PATH" ]; then #6+2
     lock_value "6" "/sys/devices/system/cpu/cpu0/core_ctl/max_cpus"
     lock_value "6" "/sys/devices/system/cpu/cpu0/core_ctl/min_cpus"
     lock_value "2" "/sys/devices/system/cpu/cpu6/core_ctl/max_cpus"
@@ -241,7 +230,7 @@ fi
 lock_value "0" "/sys/devices/system/cpu/cpu*/core_ctl/enable"
 lock_value "0" "/sys/devices/system/cpu/cpu*/core_ctl/core_ctl_boost"
 
-if [ -d /sys/class/devfreq/gpufreq/ ]; then
+if [ -d "/sys/class/devfreq/gpufreq/" ]; then
     lock_value "mali_ondemand" "/sys/class/devfreq/gpufreq/governor"
     lock_value "0" "/sys/class/devfreq/gpufreq/animation_boost"
     lock_value "0" "/sys/class/devfreq/gpufreq/cl_boost"
@@ -250,24 +239,24 @@ if [ -d /sys/class/devfreq/gpufreq/ ]; then
     lock_value "10" "/sys/class/devfreq/gpufreq/downdifferential"
     gpu_max_freq=$(get_max_freq "/sys/class/devfreq/gpufreq/available_frequencies")
     gpu_min_freq=$(get_min_freq "/sys/class/devfreq/gpufreq/available_frequencies")
-    lock_value "${gpu_max_freq}" "/sys/class/devfreq/gpufreq/max_freq"
-    lock_value "${gpu_min_freq}" "/sys/class/devfreq/gpufreq/min_freq"
+    lock_value "$gpu_max_freq" "/sys/class/devfreq/gpufreq/max_freq"
+    lock_value "$gpu_min_freq" "/sys/class/devfreq/gpufreq/min_freq"
 fi
 
 for GPU_DIR in /sys/class/devfreq/*gpu*; do
-    if [ -d ${GPU_DIR} ]; then
+    if [ -d "$GPU_DIR" ]; then
         gpu_max_freq=$(get_max_freq "${GPU_DIR}/available_frequencies")
         gpu_min_freq=$(get_min_freq "${GPU_DIR}/available_frequencies")
-        lock_value "${gpu_max_freq}" "${GPU_DIR}/max_freq"
-        lock_value "${gpu_min_freq}" "${GPU_DIR}/min_freq"
+        lock_value "$gpu_max_freq" "${GPU_DIR}/max_freq"
+        lock_value "$gpu_min_freq" "${GPU_DIR}/min_freq"
     fi
 done
 
-if [ -d /sys/class/kgsl/kgsl-3d0 ]; then
+if [ -d "/sys/class/kgsl/kgsl-3d0/" ]; then
     lock_value "0" "/sys/class/kgsl/kgsl-3d0/max_pwrlevel"
-    MIN_PWRLEVEL=$(expr $(cat /sys/class/kgsl/kgsl-3d0/num_pwrlevels) - 1)
-    lock_value "${MIN_PWRLEVEL}" "/sys/class/kgsl/kgsl-3d0/min_pwrlevel"
-    lock_value "${MIN_PWRLEVEL}" "/sys/class/kgsl/kgsl-3d0/default_pwrlevel"
+    MIN_PWRLEVEL=$(($(cat /sys/class/kgsl/kgsl-3d0/num_pwrlevels) - 1))
+    lock_value "$MIN_PWRLEVEL" "/sys/class/kgsl/kgsl-3d0/min_pwrlevel"
+    lock_value "$MIN_PWRLEVEL" "/sys/class/kgsl/kgsl-3d0/default_pwrlevel"
 fi
 
 change_task_cpuset "surfaceflinger" "top-app"
@@ -309,8 +298,7 @@ if [ ! -d /sdcard/Android/ct/ ]; then
 	echo "balance" >/sdcard/Android/ct/cur_mode.txt
 fi
 
-SCRIPT_PATH=$(dirname $0)
 #CuDaemon -R [config] [mode] [log]
-${SCRIPT_PATH}/CuDaemon -R "${SCRIPT_PATH}/config.json" "/sdcard/Android/ct/cur_mode.txt" "/sdcard/Android/ct/scheduler.log"
+"${BASE_DIR}/CuDaemon" -R "${SCRIPT_PATH}/config.json" "/sdcard/Android/ct/cur_mode.txt" "/sdcard/Android/ct/scheduler.log"
 
 exit 0
